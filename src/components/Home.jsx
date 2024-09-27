7379daecc1ed59258e47957e51db3a7ce7eb6346
Junaid <junaidsajid0051@gmail.com>
2024-09-27 14:44:44 +0500

    update cv junaid

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import pdf from "../pdf/Muhammad Junaid Mern-Stack.pdf";
+import pdf from "../pdf/Muhammad junaid [Mern Stack].pdf"
 import hero from "./data/hero.json";
 import Typed from "typed.js";
 
@@ -33,7 +33,7 @@ const Home = () => {
 
           <a
             href={pdf}
-            download="Resume.pdf"
+            download="Muhammad Junaid [Mern Stack]"
             className="btn btn-outline-warning my-3"
           >
             Download Resume
